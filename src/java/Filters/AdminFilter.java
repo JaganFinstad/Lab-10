965497f9c6a3e0e37c172f9691cdc5d850a932df
jagan <jagan@DESKTOP-HP552R3>
2021-11-28 21:03:31 -0700

    Forgot to commit before pushing oof

---
+++ src/java/Filters/AdminFilter.java
@@ -0,0 +1,51 @@
+
+package Filters;
+
+import dataaccess.UserDB;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.User;
+import dataaccess.UserDB;
+
+
+public class AdminFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        
+        UserDB userDB = new UserDB();
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpSession session = httpRequest.getSession();
+        String email = (String)session.getAttribute("email");
+        User user = userDB.get(email);
+        HttpServletResponse httpResponse = (HttpServletResponse)response;
+   
+        if (user.getRole().getRoleId() != 1) {
+        httpResponse.sendRedirect("notes");
+        return;
+   }
+        
+        chain.doFilter(request, response);
+    }
+    
+    
+
+    @Override
+    public void destroy() {
+    }
+    
+}
